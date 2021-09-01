0c8a65f545f8f1b83f07784fe38305a38a04b611
ADAM RUSAIDI <adam_rusaidi@connect.ite.edu.sg>
2021-09-01 15:18:08 +0800

    Add project files.

---
+++ WSD_Project_C2004V_4/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WSD_Project_C2004V_4.Models;
+
+namespace WSD_Project_C2004V_4.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WSD_Project_C2004V_4.Models.Customer> Customer { get; set; }
+
+        public DbSet<WSD_Project_C2004V_4.Models.Order> Order { get; set; }
+    }
+}
---
+++ WSD_Project_C2004V_4/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSD_Project_C2004V_4.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        [Required]
+        public string CustomerName { get; set; }
+        public string CustomerPassword { get; set; }
+    }
+}
